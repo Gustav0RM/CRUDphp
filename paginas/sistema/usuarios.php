3fa5664157378a6bff4c7a5baeb9a85e3fc2ecd8
Gustavo <gustavo.roube.martins@hotmail.com>
2025-07-07 22:07:53 -0300

    Ajustes

--- paginas/sistema/usuarios.php
+++ paginas/sistema/usuarios.php
@@ -1,25 +1,30 @@
+<?php 
+$tipo = isset($_GET["tipo"]) ? strtolower($_GET["tipo"]) : null;
+$titulo = ($tipo === "cad" ? "Cadastrar" : ($tipo === "alt" ? "Editar" : "Buscar")) . " Usuários"; 
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../../style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">  
-  <link rel="stylesheet" href="style.css">
-  <title>Document</title>
+  
+  <title><?=$titulo?></title>
 </head>
 <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="navegacao">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Meu Site</a>
+  <nav class="navbar navbar-expand-lg" id="navegacao" >
+    <div class="container-fluid" >
+      <a class="navbar-brand text-white" href="#">Meu Site</a>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Sistema
+              <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Sistema
               </a>
             <ul class="dropdown-menu">
               <li>
-                <a class="dropdown-item" href="../paginas/usuarios.php">Controle de usuários</a>
+                <a class="dropdown-item" href="../sistema/usuarios.php">Controle de usuários</a>
               </li>
               <li>
                 <hr class="dropdown-divider">
@@ -37,14 +42,14 @@
   </nav>
 
   <div class="container mt-4">
-    <a class="text-decoration-none text-black" href="#"> 
+    <a class="text-decoration-none text-black" href="../sistema/usuarios.php?tipo=cad"> 
       <i class="bi bi-person-add fs-5 text-success"></i>
       Cadastrar usuário
     </a>
     <div class="row">
       <div class="col-md-12">
-        <div class="card mt-3">
-          <div class="card-header">
+        <div class="card mt-3" id="cards">
+          <div class="card-header" id="card-header">
             <h4>Lista de usuários
             </h4>
           </div>
@@ -60,7 +65,7 @@
                 </tr>
                 <tr>
                   <td class="text-center">
-                    <a class="text-decoration-none" href="#">
+                    <a class="text-decoration-none" href="">
                       <i class="bi bi-person-gear text-black fs-5"></i>
                     </a>
                   </td>
@@ -80,8 +85,6 @@
       </div>
     </div>
   </div>
-
-
 
 
 
