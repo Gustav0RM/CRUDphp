b4623957f5eab5dac3fe6eaa5cc981696439ed28
Gustavo <gustavo.roube.martins@hotmail.com>
2025-07-09 11:38:07 -0300

    Correção

--- acoes/salvar.php
+++ acoes/salvar.php
@@ -4,8 +4,21 @@ require '../classes/conexao.php';
 
 if (isset($_POST['cadbd']))
   {
-  $nome = mysqli_real_escape_string($conexao, trim($_POST['nome']));
-  $senha = isset($_POST['senha']) ? mysqli_real_escape_string($conexao, password_hash(trim($_POST['senha']), PASSWORD_DEFAULT)) : '';
+  $nome_limpo = isset($_POST['nome']) ? trim($_POST['nome']) : '';
+  $senha_limpa = isset($_POST['senha']) ? trim($_POST['senha']) : '';
+  $senha = '';
+  $nome = '';
+
+  if (empty($nome_limpo) || empty($senha_limpa))
+    {
+    $_SESSION['mensagem'] = "CADASTRO NAO REALIZADO. Os campos nome e senha são obrigatórios";
+    header('Location: ../paginas/sistema/usuarios.php?tipo=');
+    exit;
+    }
+  
+  $nome = mysqli_escape_string($conexao, $nome_limpo); 
+  $senha = password_hash($senha_limpa, PASSWORD_DEFAULT);
+
 
   $sql = "INSERT INTO tb_usuario (nome_usu,senha_usu) VALUES ('$nome', '$senha')";
 
--- paginas/sistema/usuarios.php
+++ paginas/sistema/usuarios.php
@@ -6,7 +6,8 @@
   <link rel="stylesheet" href="../../style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css"> 
-  <?php 
+  <?php
+  session_start(); 
   $tipo = isset($_GET["tipo"]) ? strtolower($_GET["tipo"]) : null;
   $titulo = ($tipo === "cad" ? "Cadastrar" : ($tipo === "alt" ? "Editar" : "Buscar")) . " Usuário"; ?>
   <title><?=$titulo?></title>
@@ -41,7 +42,7 @@
       <?php
       if (!$tipo)
         { 
-         include('../../acoes/mensagem.php'); //não funciona ?>
+         include('../../acoes/mensagem.php'); ?>
         <div class="container mt-4 ms-0">
           <a class="text-decoration-none text-black" href="../sistema/usuarios.php?tipo=cad"> 
             <i class="bi bi-person-add fs-5 text-success"></i>
